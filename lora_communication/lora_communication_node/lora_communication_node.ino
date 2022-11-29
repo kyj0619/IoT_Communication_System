d4c4293ba279934d993f24fca82749ddd7894a49
kyj0619 <kyj97yj@gmail.com>
2022-11-29 11:48:17 +0900

    Update lora_communication_node.ino
    ardunio node code 수정

--- lora_communication/lora_communication_node/lora_communication_node.ino
+++ lora_communication/lora_communication_node/lora_communication_node.ino
@@ -1,37 +1,6 @@
 #include <SoftwareSerial.h>
 #include "SNIPE.h"
 
-//firebase 접속시 필요한 정보들
-#if defined(ESP32)//insert code start
-#include <WiFi.h>
-#include <FirebaseESP32.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <FirebaseESP8266.h>
-#endif
-#include <addons/TokenHelper.h>
-#include <addons/RTDBHelper.h>
-
-#define WIFI_SSID "AndroidHotspot6619"
-#define WIFI_PASSWORD "1234567890"
-
-#define API_KEY "AIzaSyAmXNIA1w7ctrHYQv1tH_yIQaHeqPikCCc"
-
-#define DATABASE_URL "test-f8051-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
-
-#define USER_EMAIL "kyj97yj@gmail.com"
-#define USER_PASSWORD "dkssud454117858"
-
-FirebaseData fbdo;
-
-FirebaseAuth auth;
-FirebaseConfig config;
-
-unsigned long sendDataPrevMillis = 0;
-
-unsigned long count = 0;//insert code end
-
-
 #define CENTER 0
 #define FIRE 1
 #define HEART 2
@@ -52,37 +21,6 @@ SoftwareSerial DebugSerial(RXpin,TXpin);
 SNIPE SNIPE(ATSerial);
 void setup() {
    ATSerial.begin(115200);
-
-//wifi연결하여 firebase로 접속하는 과정
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);//insert code start
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-
-  config.api_key = API_KEY;
-
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  config.database_url = DATABASE_URL;
-
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-  Firebase.begin(&config, &auth);
-
-  Firebase.reconnectWiFi(true);
-
-  Firebase.setDoubleDigits(5);//insert code end
-
 
   // put your setup code here, to run once:
   while(ATSerial.read()>= 0) {}
@@ -131,20 +69,6 @@ void loop() {
       if(SNIPE.lora_send("Heart send")) // 심박 노드 보내라
       {
         DebugSerial.println("Let Heart send");
-
-//firebase로 gps 데이터를 넘길때
-    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
-  {
-    sendDataPrevMillis = millis();
-    
-    Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/Data/gps"), recv) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/Data/gps")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-
-    FirebaseJson json;//insert code end
-  }
-
-        
         stat = 1;
       }
     }
@@ -153,20 +77,6 @@ void loop() {
       if(SNIPE.lora_send("Fire send")) // 화재경보 노드 보내라
       {
         DebugSerial.println("Let Fire send");
-
-//firebase로 심박수 데이터를 넘길때
-    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
-  {
-    sendDataPrevMillis = millis();
-    
-    Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/Data/heartbeat"), recv) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/Data/heartbeat")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-
-    FirebaseJson json;//insert code end
-  }
-
-        
         stat = 0;
       }
     }
