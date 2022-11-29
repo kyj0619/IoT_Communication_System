fae8fcd806f04c954ba7508ae34d06d7b7bfb9b2
kyj0619 <kyj97yj@gmail.com>
2022-11-29 11:40:49 +0900

    Update lora_communication_gateway.ino
    arduino gateway code 수정

--- lora_communication/lora_communication_gateway/lora_communication_gateway.ino
+++ lora_communication/lora_communication_gateway/lora_communication_gateway.ino
@@ -1,8 +1,7 @@
 #include <SoftwareSerial.h>
 #include "SNIPE.h"
 
-
-
+//firebase 접속시 필요한 정보들
 #if defined(ESP32)//insert code start
 #include <WiFi.h>
 #include <FirebaseESP32.h>
@@ -33,28 +32,28 @@ unsigned long sendDataPrevMillis = 0;
 unsigned long count = 0;//insert code end
 
 
-
 #define CENTER 0
 #define FIRE 1
 #define HEART 2
 #define MOBILE 3  // For wifi send
 
-#define THIS_NODE CENTER    /* Please define what this node */
+#define THIS_NODE HEART    /* Please define what this node */
 
-#define TXpin D9 //11 or D9
-#define RXpin D8 //10 or D8
+#define TXpin 11 //11 or D9
+#define RXpin 10 //10 or D8
 #define ATSerial Serial
 
 //16byte hex key
 String lora_app_key = "22 11 33 44 55 66 77 88 99 aa bb cc dd ee ff 00";  
+
+int stat = 0;
 
 SoftwareSerial DebugSerial(RXpin,TXpin);
 SNIPE SNIPE(ATSerial);
 void setup() {
    ATSerial.begin(115200);
 
-
-
+//wifi연결하여 firebase로 접속하는 과정
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);//insert code start
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -83,7 +82,6 @@ void setup() {
   Firebase.reconnectWiFi(true);
 
   Firebase.setDoubleDigits(5);//insert code end
-
 
 
   // put your setup code here, to run once:
@@ -126,63 +124,116 @@ void setup() {
 
 void loop() {
 
-
-//   if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
-//  {
-//    sendDataPrevMillis = millis();
-//    
-//  Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/test/string"), "Hello World!") ? "ok" : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/test/string")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-//
-//    FirebaseJson json;//insert code end
-//  }
- 
-
-  
 #if THIS_NODE == CENTER
-    String recv = SNIPE.lora_recv();
-    delay(300);
 
-    DebugSerial.println(recv);
+    if(stat == 0) // 직전에 화재 경보 받았다
+    {
+      if(SNIPE.lora_send("Heart send")) // 심박 노드 보내라
+      {
+        DebugSerial.println("Let Heart send");
 
-
-    
-   if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
+//firebase로 gps 데이터를 넘길때
+    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
   {
     sendDataPrevMillis = millis();
     
-    Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/test/string"), recv) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/Data/gps"), recv) ? "ok" : fbdo.errorReason().c_str());
 
-    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/test/string")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
+    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/Data/gps")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
 
     FirebaseJson json;//insert code end
   }
+
+        
+        stat = 1;
+      }
+    }
+    else if(stat == 1) // 직전에 심박 데이터 받았다
+    {
+      if(SNIPE.lora_send("Fire send")) // 화재경보 노드 보내라
+      {
+        DebugSerial.println("Let Fire send");
+
+//firebase로 심박수 데이터를 넘길때
+    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))//insert code start
+  {
+    sendDataPrevMillis = millis();
     
+    Serial.printf("Set string... %s\n", Firebase.setString(fbdo, F("/Data/heartbeat"), recv) ? "ok" : fbdo.errorReason().c_str());
 
-    if(SNIPE.lora_send("end"))
-    {
-      DebugSerial.println("send success");
+    Serial.printf("Get string... %s\n", Firebase.getString(fbdo, F("/Data/heartbeat")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
+
+    FirebaseJson json;//insert code end
+  }
+
+        
+        stat = 0;
+      }
     }
+    
+    String recv = SNIPE.lora_recv();
+    if (recv != "AT_RX_TIMEOUT" && recv != "AT_RX_ERROR")
+    {
+      DebugSerial.println(recv);
+      DebugSerial.println(SNIPE.lora_getRssi());
+      DebugSerial.println(SNIPE.lora_getSnr());            
+    }
+    else
+    {
+      DebugSerial.println("recv fail");
+      delay(500);
+    }
+
+    
     delay(1000);
 
-#elif THIS_NODE == FIRE
-    String data = "gps";
-    if(SNIPE.lora_send(data))
+#elif THIS_NODE == FIRE  // "Fire send" 받았으면 데이터를 보낸다.
+    String data = "gpsx,gpsy";
+    String recv = SNIPE.lora_recv();
+
+    if (recv != "AT_RX_TIMEOUT" && recv != "AT_RX_ERROR")
     {
-      DebugSerial.println("send success");
+      DebugSerial.println(recv);
+      if(recv == "Fire send")
+      {
+        if(SNIPE.lora_send(data))
+       {
+        DebugSerial.println("send success, fire node");
+         //delay(1000);
+       }
+        else
+       {
+        DebugSerial.println("send fail, fire node");
+        delay(500);
+       }     
+      }
     }
+    
+   
 
-    delay(1000);
-
-#elif THIS_NODE == HEART
+#elif THIS_NODE == HEART // "Heart send" 받았으면 데이터를 보낸다.
     String data = "heart";
-    if(SNIPE.lora_send(data))
+    String recv = SNIPE.lora_recv();
+    
+   if (recv != "AT_RX_TIMEOUT" && recv != "AT_RX_ERROR")
     {
-      Debug.Serial.println("send success");
+      DebugSerial.println(recv);
+      if(recv == "Heart send")
+      {
+        if(SNIPE.lora_send(data))
+       {
+        DebugSerial.println("send success, heart node");
+        //delay(1000);
+       }
+        else
+       {
+        DebugSerial.println("send fail, heart node");
+        delay(500);
+       }     
+      }
     }
 
-    delay(1000);
+    
 
 #endif
 }
